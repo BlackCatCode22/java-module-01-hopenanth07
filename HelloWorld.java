024b5e8eb0a548aa1e3b71b7faa9391aef454a29
Hope Nantha <hopenanth@gmail.com>
2025-08-12 12:01:46 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// HelloWorld.java
+// Hope Nanthavongdouangsy 8/12/2025
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+    }
+}
